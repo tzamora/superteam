a297c5df0a305a32e9937230af9c09e2939c40a3
unknown <estudiante@lab211-03.campus.uveritas.ac.cr>
2016-01-31 14:18:32 -0600

    k

---
+++ Assets/TriggerRespawnController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerRespawnController : MonoBehaviour {
+
+	public Transform respawnPoint;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.GetComponent<SuperCarController> () != null) {
+			other.transform.position = respawnPoint.position;
+		}
+	}
+}
